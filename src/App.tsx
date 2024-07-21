d160bd8680dd6a9909e46030e258d801a2fcbb1b
Aviv Turgeman <aturgema@redhat.com>
2024-07-21 20:01:42 +0300

    Remove router
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 // src/App.tsx
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import EndGame from "./components/EndGame/EndGame";
 import styles from "./App.module.css";
@@ -22,13 +21,7 @@ const Home: React.FC = () => {
 };
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+  return <Home />;
 };
 
 export default App;
