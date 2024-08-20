f69bff68923e68c6b990de917542e2f586d29631
Aviv Turgeman <aturgema@redhat.com>
2024-08-20 17:55:03 +0300

    Add remove rebuy
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/components/PlayerTable/PlayerTable.tsx
+++ src/components/PlayerTable/PlayerTable.tsx
@@ -13,15 +13,12 @@ import {
 import AddIcon from "@mui/icons-material/Add";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import RemoveIcon from "@mui/icons-material/Remove";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 const PlayerTable: React.FC = () => {
   const { players, updateOwed, usedShowMe, quitPlayer } = usePlayerStore();
   const { defaultRebuy } = useSettingsStore();
-
-  const handleAddAmount = (index: number, amount: number) => {
-    updateOwed(index, amount);
-  };
 
   const handleQuitPlayer = (index: number) => {
     const finalResult = prompt("Enter final amount");
@@ -61,10 +58,17 @@ const PlayerTable: React.FC = () => {
                 </IconButton>
                 <IconButton
                   color="primary"
-                  onClick={() => handleAddAmount(index, defaultRebuy)}
+                  onClick={() => updateOwed(index, defaultRebuy)}
                   disabled={player.hasQuit}
                 >
                   <AddIcon />
+                </IconButton>
+                <IconButton
+                  color="primary"
+                  onClick={() => updateOwed(index, defaultRebuy * -1)}
+                  disabled={player.hasQuit || player.owed === defaultRebuy}
+                >
+                  <RemoveIcon />
                 </IconButton>
               </TableCell>
               <TableCell align="right">
