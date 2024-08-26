a5bd77fdb9a2aa886f518026535201c4521f6dda
Aviv Turgeman <aturgema@redhat.com>
2024-08-26 08:11:41 +0300

    Fix avatar image
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/components/PlayerTable/PlayerRow.tsx
+++ src/components/PlayerTable/PlayerRow.tsx
@@ -9,7 +9,7 @@ const PlayerRow: FC<PlayerRowProps> = ({ player, index }) => {
     <TableRow key={player.id}>
       <TableCell>
         <Stack direction="row" gap={2}>
-          <Avatar alt="Random fish" src={`/assets/airship_8315199.png`} />{" "}
+          <Avatar alt="Random fish" src={`/assets/${fishMapper[index]}.png`} />{" "}
           <div style={{ display: "flex", alignItems: "center" }}>
             {player.name}
           </div>
